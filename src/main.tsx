94705655a18505ad1a0195e972883eda65dfb530
Andres Bohorquez <acbohorquezg1003jt@gmail.com>
2023-08-21 21:30:49 -0500

    refactor: refactor code and change extension to tsx

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GifExpertApp } from './gifExpertApp';
+
+
+import './styles.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
