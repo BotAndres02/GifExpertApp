14d5f504887d55e0d9d843151ffde4ffa7fd3600
Andres Bohorquez <acbohorquezg1003jt@gmail.com>
2023-08-21 21:27:50 -0500

    fix: fix the code and bring the info from the API

--- src/helpers/getGifs.ts
+++ src/helpers/getGifs.ts
@@ -1,14 +1,27 @@
-export const getGifs = async( category ) => {
+
+
+
+interface GiphyImageData {
+    id: string;
+    title: string;
+    images: {
+        downsized_medium: {
+            url: string;
+        };
+    };
+}
+
+export const getGifs = async( category: string ) => {
 
     const url = `https://api.giphy.com/v1/gifs/search?api_key=IFz5qZkysYiZgNSFkwm7x3FeGZzB01es&q=${category}&limit=5`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
-    const gifs = data.map( img => ({
+    const gifs = data.map( ( img: GiphyImageData ) => ({
         id: img.id,
         title: img.title,
         url: img.images.downsized_medium.url
     }));
     
-    return gifs;
+    return gifs;   
 }
